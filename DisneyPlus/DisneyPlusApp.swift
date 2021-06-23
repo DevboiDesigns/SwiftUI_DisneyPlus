1a44b392d22beba09843fb48fde5498372497457
DevboiDesigns <visrule@protonmail.com>
2021-06-23 10:29:45 -0500

    Initial Commit

---
+++ DisneyPlus/DisneyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyPlusApp.swift
+//  DisneyPlus
+//
+//  Created by Christopher Hicks on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct DisneyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
